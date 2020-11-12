07e5a9fd177d0e859714f9b65ca1343d674cdfdb
starseeker <54729715+whlGitHub@users.noreply.github.com>
2020-11-12 20:39:14 +0800

    蓝桥杯国赛

--- main.c
+++ main.c
@@ -53,6 +53,9 @@ void Freq_Timer1(void);
 uint PWM_NUM;
 void PWM_Init();
 
+unsigned int Check_Distance();
+void send_wave(void) ;
+
 void main()
 {
 	All_Init();
@@ -61,7 +64,7 @@ void main()
 	ET0 = 1;
 	while(1)
 	{	
-		printf("Frenquence: %d\r\n",freq);
+
 	}
 	
 }
@@ -603,4 +606,60 @@ void PWM_Output(void)	interrupt 1
 			PWM_NUM = 0;
 		}
 	}
+}
+
+#define somenops {_nop_(); _nop_(); _nop_(); _nop_(); _nop_(); _nop_(); _nop_(); _nop_(); _nop_(); _nop_();}
+
+sbit TX = P1^0; //发射引脚
+sbit RX = P1^1; //接收引脚
+
+//TX 引脚发送 40KHz 方波信号驱动超声波发送探头
+
+void send_wave(void) 
+{
+	unsigned char i = 8; //发送 8 个脉冲
+	
+	do {
+		TX = 1;
+		somenops;somenops;somenops;somenops;somenops;  
+		somenops;somenops;somenops;somenops;somenops;
+		TX = 0;
+		somenops;somenops;somenops;somenops;somenops;  
+		somenops;somenops;somenops;somenops;somenops;
+	} while(i--);
+}
+
+unsigned int Check_Distance()
+{
+	unsigned int distance,t;
+
+    TMOD &= 0xF0;  //配置定时器工作模式
+    TH0 = 0;
+    TL0 = 0;  
+  
+    EA = 1;
+    ET0 = 0;  //打开定时器0中断
+    //TR0 = 1;  //启动定时器   
+
+	send_wave();  //发送方波信号
+	TR0= 1;  //启动计时
+	while((RX == 1) && (TF0 == 0));  //等待收到脉冲
+		 TR0 = 0;  //关闭计时
+
+	//发生溢出
+	if(TF0 == 1)
+	{
+		TF0= 0;
+		distance = 999;  //无返回
+	}
+	else
+	{
+		/**  计算时间  */
+		t = (TH0<<8)|TL0;
+		distance = (unsigned int)(t*0.017);  //计算距离				
+		//distance = (unsigned int)(t*17/1000);  //计算距离				
+	}
+	TH0 = 0;
+	TL0 = 0;
+	return distance;
 }
